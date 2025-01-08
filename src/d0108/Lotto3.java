0c05f0c76800fb54f5ef6ac1fce1630e5be37e97
hblim <hblim@NB_HB>
2025-01-08 12:39:05 +0900

    ddd

--- src/d0108/Lotto3.java
+++ src/d0108/Lotto3.java
@@ -1,8 +1,20 @@
 package d0108;
 
+import java.util.Random;
+
 public class Lotto3 {
 	public static void main(String[] args) {
+		int[] nums = new int[3];
+		Random r = new Random();
 		
+		
+		nums[0] = r.nextInt(100);
+		nums[1] = r.nextInt(100);
+		nums[2] = r.nextInt(100);
+		
+		
+		System.out.println(nums[0]);
+		System.out.println(nums[1]);
+		System.out.println(nums[2]);
 	}
-
 }
